3ecd9342861f94787cbf1b0fe89be9320459fa05
Ichiniu <ikhsanwahyu04@gmail.com>
2026-01-05 08:35:17 +0700

    .

--- application/controllers/admin/Login.php
+++ application/controllers/admin/Login.php
@@ -5,10 +5,10 @@
  */
 class Login extends CI_Controller
 {
-
 	public function __construct()
 	{
 		parent::__construct();
+		$this->load->library(['session', 'form_validation']);
 	}
 
 	function index()
@@ -18,39 +18,36 @@ class Login extends CI_Controller
 
 	public function is_sign_in()
 	{
-		$this->load->library('form_validation');
-
 		$this->form_validation->set_rules('username', 'Username', 'required');
 		$this->form_validation->set_rules('password', 'Password', 'required');
 
-		$username = $this->input->post('username');
-		$password = $this->input->post('password');
+		$username = $this->input->post('username', TRUE);
+		$password = $this->input->post('password', TRUE);
 
 		if ($this->form_validation->run() == FALSE) {
-
 			$data['error'] = "Username dan Password harus diisi!";
 			$this->load->view('admin/admin_login', $data);
-		} else {
-
-			if ($username == 'admin' && $password == 'admin') {
-
-				$session = array(
-					'admin_username'   => $username,
-					'admin_logged_in'  => TRUE,
-					'admin_session_id' => session_id()
-				);
-
-				$this->session->set_userdata($session);
-				redirect('admin/dashboard');
-			} else {
-
-				$data['error'] = "USERNAME / PASSWORD YANG ANDA MASUKAN SALAH!";
-				$this->load->view('admin/admin_login', $data);
-			}
+			return;
 		}
-	}
 
-	function log_out()
+		if ($username === 'admin' && $password === 'admin') {
+
+			$this->session->set_userdata([
+				'admin_username'   => $username,
+				'admin_logged_in'  => TRUE,
+				'admin_session_id' => session_id()
+			]);
+
+			redirect('admin/dashboard');
+			return;
+		}
+
+		$data['error'] = "USERNAME / PASSWORD YANG ANDA MASUKAN SALAH!";
+		$this->load->view('admin/admin_login', $data);
+	}
+}
+
+function log_out()
 	{
 		// Jangan sess_destroy() karena itu akan ngehapus session user juga
 		$this->session->unset_userdata(['admin_username', 'admin_logged_in', 'admin_session_id']);
