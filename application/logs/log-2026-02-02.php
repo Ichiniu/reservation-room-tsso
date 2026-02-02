64a7c4fc9349921c6b9345662b6623ac1e83d087
Ichiniu <ikhsanwahyu04@gmail.com>
2026-02-02 08:59:08 +0700

    ..

---
+++ application/logs/log-2026-02-02.php
@@ -0,0 +1,24 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2026-02-02 02:58:48 --> Config Class Initialized
+INFO - 2026-02-02 02:58:48 --> Hooks Class Initialized
+DEBUG - 2026-02-02 02:58:48 --> UTF-8 Support Enabled
+INFO - 2026-02-02 02:58:48 --> Utf8 Class Initialized
+INFO - 2026-02-02 02:58:48 --> URI Class Initialized
+DEBUG - 2026-02-02 02:58:49 --> No URI present. Default controller set.
+INFO - 2026-02-02 02:58:49 --> Router Class Initialized
+INFO - 2026-02-02 02:58:49 --> Output Class Initialized
+INFO - 2026-02-02 02:58:49 --> Security Class Initialized
+DEBUG - 2026-02-02 02:58:49 --> Global POST, GET and COOKIE data sanitized
+INFO - 2026-02-02 02:58:49 --> Input Class Initialized
+INFO - 2026-02-02 02:58:49 --> Language Class Initialized
+INFO - 2026-02-02 02:58:49 --> Loader Class Initialized
+INFO - 2026-02-02 02:58:49 --> Helper loaded: url_helper
+INFO - 2026-02-02 02:58:49 --> Database Driver Class Initialized
+INFO - 2026-02-02 02:58:49 --> Session: Class initialized using 'files' driver.
+INFO - 2026-02-02 02:58:49 --> Controller Class Initialized
+INFO - 2026-02-02 02:58:49 --> Model Class Initialized
+INFO - 2026-02-02 02:58:49 --> Model Class Initialized
+INFO - 2026-02-02 02:58:49 --> File loaded: C:\xampp\htdocs\bookingsmarts\application\views\welcome_screen.php
+INFO - 2026-02-02 02:58:49 --> Final output sent to browser
+DEBUG - 2026-02-02 02:58:49 --> Total execution time: 0.4274
