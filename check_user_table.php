6bf427002c88d45c36aaa0a396cf33810deba6e8
Ichiniu <ikhsanwahyu04@gmail.com>
2026-02-12 14:29:56 +0700

    ..

--- check_user_table.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = mysqli_connect('localhost', 'root', '', 'bookingsmarts');
-$res = mysqli_query($conn, "DESCRIBE user");
-echo "User Table:\n";
-while ($row = mysqli_fetch_assoc($res)) {
-    print_r($row);
-}
-mysqli_close($conn);
